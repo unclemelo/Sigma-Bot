229392dc708f9f7858c202aa3df35aa3f2f33217
rumiz123 <nrumiz@icloud.com>
2025-02-06 16:33:34 -0800

    guess.py revamp

--- cogs/guess.py
+++ cogs/guess.py
@@ -1,62 +1,31 @@
 import random
 import asyncio
+import discord
 from discord import app_commands
 from discord.ext import commands
-
 
 class Guess(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @app_commands.command(name="guess", description="the guess game")
-    async def on_message(self, message):
-        if message.author.id == self.user.id:
-            return
+    @app_commands.command(name="guess", description="Guess a number between 1 and 10")
+    async def guess(self, interaction: discord.Interaction):
+        answer = random.randint(1, 10)
 
-        if message.content.startswith('$guess'):
-            await message.channel.send('Guess a number between 1 and 10.')
+        await interaction.response.send_message("Guess a number between 1 and 10.")
 
-            def is_correct(m):
-                return m.author == message.author and m.content.isdigit()
+        def is_correct(m: discord.Message):
+            return m.author == interaction.user and m.content.isdigit()
 
-            answer = random.randint(1, 10)
+        try:
+            guess = await self.bot.wait_for("message", check=is_correct, timeout=5.0)
+        except asyncio.TimeoutError:
+            return await interaction.followup.send(f"Sorry, you took too long! The answer was {answer}.")
 
-            try:
-                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
-            except asyncio.TimeoutError:
-                return await message.channel.send(f'Sorry, you took too long it was {answer}.')
-
-            if int(guess.content) == answer:
-                await message.channel.send('You are right!')
-            else:
-                await message.channel.send(f'Oops. It is actually {answer}.')
-
+        if int(guess.content) == answer:
+            await interaction.followup.send("You are right!")
+        else:
+            await interaction.followup.send(f"Oops! The correct answer was {answer}.")
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Guess(bot))
-
-
-# class MyClient(discord.Client):
-#     async def on_message(self, message):
-#         # we do not want the bot to reply to itself
-#         if message.author.id == self.user.id:
-#             return
-#
-#         if message.content.startswith('$guess'):
-#             await message.channel.send('Guess a number between 1 and 10.')
-#
-#             def is_correct(m):
-#                 return m.author == message.author and m.content.isdigit()
-#
-#             answer = random.randint(1, 10)
-#
-#             try:
-#                 guess = await self.wait_for('message', check=is_correct, timeout=5.0)
-#             except asyncio.TimeoutError:
-#                 return await message.channel.send(f'Sorry, you took too long it was {answer}.')
-#
-#             if int(guess.content) == answer:
-#                 await message.channel.send('You are right!')
-#             else:
-#                 await message.channel.send(f'Oops. It is actually {answer}.')
-
