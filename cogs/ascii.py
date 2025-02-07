dfb2bf44ce95388b98404e0c16e8b90967acfeb9
rumiz123 <nrumiz@icloud.com>
2025-02-06 17:11:29 -0800

    oh yea we got ascii now

--- cogs/ascii.py
+++ cogs/ascii.py
@@ -6,33 +6,99 @@ class AsciiGroup(app_commands.Group):
     def __init__(self):
         super().__init__(name="ascii", description="ASCII art commands")
 
-    @app_commands.command(name="val", description="Generate ASCII art or perform actions based on choices")
+    @app_commands.command(name="val", description="Generate ASCII art to paste into val chat")
     @app_commands.choices(
         text=[
             app_commands.Choice(name="-20rr", value="-20rr"),
-            app_commands.Choice(name="-15zz", value="-15zz"),
-            app_commands.Choice(name="-42xy", value="-42xy"),
+            app_commands.Choice(name="uninstall", value="uninstall"),
+            app_commands.Choice(name="middlefinger", value="middlefinger"),
+            app_commands.Choice(name="nerd", value="nerd"),
+            app_commands.Choice(name="dontshoot", value="dontshoot"),
+            app_commands.Choice(name="nice", value="nice"),
+            app_commands.Choice(name="ggwp", value="ggwp"),
+            app_commands.Choice(name="banhammer", value="banhammer"),
+            app_commands.Choice(name="afk", value="afk"),
+            app_commands.Choice(name="urmom", value="urmom"),
+            app_commands.Choice(name="hmmm", value="hmmm"),
+            app_commands.Choice(name="lol", value="lol"),
+            app_commands.Choice(name="ace", value="ace"),
         ]
     )
     async def val(self, interaction: discord.Interaction, text: str):
         if text == "-20rr":
             ascii_art = """\
-――――――▄▌▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌――-- ▄▄██▌█ BEEP BEEP ―――――― ▄▄▄▌▐██▌█ -20rr delivery 
-███████▌█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌▀(@)▀▀▀▀▀(@)(@)▀▀▀▀▀▀▀(@)(@)▀▀▘
+――――――▄▌▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌――-- ▄▄██▌█ BEEP BEEP ―――――― ▄▄▄▌▐██▌█ -20rr delivery ███████▌█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌▀(@)▀▀▀▀▀(@)(@)▀▀▀▀▀▀▀(@)(@)▀▀▘
 """
             await interaction.response.send_message(ascii_art)
-#         elif text == "-15zz":
-#             ascii_art_2 = """\
-# -- ASCII Art for -15zz --
-# (ASCII art or response for -15zz)
-# """
-#             await interaction.response.send_message(ascii_art_2)
-#         elif text == "-42xy":
-#             ascii_art_3 = """\
-# -- ASCII Art for -42xy --
-# (ASCII art or response for -42xy)
-# """
-#             await interaction.response.send_message(ascii_art_3)
+        elif text == "uninstall":
+            ascii_art = """\
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░UNINSTALLING░VALORANT░░░░░░░░░░░░░▇▇▇▇▇▇▇▇▇▇▇▇▇▇▢░░░░░░░░░░░░░╭━╮╭━╮╭╮░╱░░░░░░░░░░░░░░░░╰━┫╰━┫╰╯╱╭╮░░░░░░░░░░░░░░░╰━╯╰━╯░╱░╰╯░░░░░░░░░░░░░░░░░COMPLETE░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+"""
+            await interaction.response.send_message(ascii_art)
+        elif text == "middlefinger":
+            ascii_art = """\
+▒▒▒▒▒▒▒▒▒▒▒▒▒█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒███████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒██▀▀█████▀▀██▒▒▒▒▒▒▒▒▒▒▒▒█████▄███▄█████▒▒▒▒▒█▒▒▒▒▐██░░░█████░░░██▌▒▒▒▒█▒▒▒▒▐██■░░█████■░░██▌▒▒▒▒█▒▒▒▒▐███████████████▌▒▒▒███▒▒▒▐███████████████▌▒▒▐███▒▒▒▐████▀▀▀▀▀▀▀████▌▒▒▐███▌▒▒▐███▌■■■■■■■▐███▌▒▒▒███▌▒▒▒███▌■■■■■■■▐███▒▒▒▒▒█████▌▒███▄▄▄▄▄▄▄███▒▒▒▒▒▒▒█████▒▒███████████▒▒▒
+"""
+            await interaction.response.send_message(ascii_art)
+        elif text == "nerd":
+            ascii_art = """\
+────────────████████────── ───────────██████████───── ──────────█▄▄▄████▄▄▄█──── ─────▄───█░░░░░██░░░░░█─── ─────█──░░░▓■▓░░░░▓■▓░░░── ─────█──▐█░▓▓▓░██░▓▓▓░█▌── ─────█──▐██░░░████░░░██▌── ─────█──▐█▛██████████▛█▌── ──▌▌▌▄▄──██▚████████▞██─── ──▌▌▌▀█──████▀████▀████─── ──▄▄▄██───████▄■■▄████──── ──▜███▛────██████████───── ───███──────████████──────
+"""
+            await interaction.response.send_message(ascii_art)
+        elif text == "dontshoot":
+            ascii_art = """\
+────────────────────────────────────██████────────────▐──────██▀██▀██──────▌───▐▐▐────█▄▄████▄▄█────▌▌▌─▐▐▐▐───▐█░■░██░■░█▌───▌▌▌▌▐███─▄▌▐█░░░██░░░█▌▐▄─███▌▐████▀─▐██████████▌─▀████▌▐███───▐██████████▌───███▌─███───▐████──████▌───███─────────████──████─────────────────████████───────────────────▀▀▀▀▀▀────────────────────────────────────
+"""
+
+            await interaction.response.send_message(ascii_art)
+        elif text == "nice":
+            ascii_art = """\
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█▄░█░▀█▀░█▀▀░█▀▀░█░░░░░░░░█░▀█░░█░░█░░░█▀▀░▀░░░░░░░░▀░░▀░▀▀▀░▀▀▀░▀▀▀░▀░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+"""
+
+            await interaction.response.send_message(ascii_art)
+        elif text == "ggwp":
+            ascii_art = """\
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█▀▀▀░█▀▀▀░█▐▌█░█▀█░░░░░░░░█░▀█░█░▀█░█▐▌█░█▀▀░░░░░░░░▀▀▀▀░▀▀▀▀░░▀▀░░▀░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+"""
+
+            await interaction.response.send_message(ascii_art)
+        elif text == "banhammer":
+            ascii_art = """\
+░░░░░░░░░░░░░░░░░░░░░░░░░░▄████▄░░░░░░░░░░░░░░░░░░░░██████▄░░░░░░▄▄▄░░░░░░░░░░░███▀▀▀▄▄▄▀▀▀░░░░░░░░░░░░░░░░▄▀▀▀▄░░░█▀▀▄░▄▀▀▄░█▄░█░░░░▄▄████░░█▀▀▄░█▄▄█░█▀▄█░░░░░██████░█▄▄▀░█░░█░█░▀█░░░░░░▀▀▀▀░░░░░░░░░░░░░░░░░
+"""
+
+            await interaction.response.send_message(ascii_art)
+        elif text == "afk":
+            ascii_art = """\
+░░█████╗░███████╗██╗░░██╗░░██╔══██╗██╔════╝██║░██╔╝░░███████║█████╗░░█████═╝░░░██╔══██║██╔══╝░░██╔═██╗░░░██║░░██║██║░░░░░██║░╚██╗░░╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝░
+"""
+
+            await interaction.response.send_message(ascii_art)
+        elif text == "ace":
+            ascii_art = """\
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█▀█░░█▀▀▀░█▀▀░░░░░░░░░░░░█▀▀▀█░█░░░   █▀▀░░░░░░░░░░░░▀░░░▀░▀▀▀▀░▀▀▀░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+"""
+
+            await interaction.response.send_message(ascii_art)
+        elif text == "urmom":
+            ascii_art = """\
+░░░░░░░░░░░░░░░░░░░░░░░░░░░█░█░█▀█░░█▄░▄█░█▀█░█▄░▄█░░█░█░██▀░░█░▀░█░█░█░█░▀░█░░▀▀▀░▀░▀░░▀░░░▀░▀▀▀░▀░░░▀░░░░░░░░░░░░░░░░░░░░░░░░░░░
+"""
+
+            await interaction.response.send_message(ascii_art)
+        elif text == "hmmm":
+            ascii_art = """\
+▒▒▒▒▒▒▒▒▒▄▄▄▄▄▄▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄█▀▀░░░░░░▀▀█▄▒▒▒▒▒▒▒▒▒▒▄█▀▄██▄░░░░░░░░▀█▄▒▒▒▒▒▒▒█▀░▀░░▄▀░░░░▄▀▀▀▀░▀█▒▒▒▒▒█▀░░░░███░░░░▄█▄░░░░▀█▒▒▒▒█░░░░░░▀░░░░░▀█▀░░░░░█▒▒▒▒█░░░░░░░░░░░░░░░░░░░░█▒▒▒▒█░░██▄░░▀▀▀▀▄▄░░░░░░░█▒▒▒▒▀█░█░█░░░▄▄▄▄▄░░░░░░█▀▒▒▒▒▒▀█▀░▀▀▀▀░▄▄▄▀░░░░▄█▀▒▒▒▒▒▒▒█░░░░░░▀█░░░░░▄█▀▒▒▒▒▒▒▒▒▒█▄░░░░░▀█▄▄▄█▀▀▒▒▒▒▒▒▒▒▒▒▒▒▀▀▀▀▀▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+"""
+
+            await interaction.response.send_message(ascii_art)
+        elif text == "lol":
+            ascii_art = """\
+────────────────────────────────────██████───────────██──────█▀▀██▀▀█───██─────██─────██████████──██─────██────▐█■░░██░░░█▌─██─────██────▐█░░░██░░■█▌─██─────██────▐██████████▌─██─────██────▐██████████▌─██─────██────▐██▀████▀██▌─██─────█████──███▄▒▒▄███──█████──█████───███▒▒███───█████───────────▀▀▀▀▀▀────────────────────────────────────
+"""
+
+            await interaction.response.send_message(ascii_art)
         else:
             await interaction.response.send_message("Invalid choice!", ephemeral=True)
 
